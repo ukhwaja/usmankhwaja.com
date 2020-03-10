886d962781ab31e2027797fa6c966b3e20c7b220
Usman Khwaja <u.khwaja@gmail.com>
2020-03-10 13:47:08 +0500

    update hugo

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -27,47 +27,47 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f17ef6411f59009592baad027a25206a"
+    "revision": "62e1341a919d2ac259d41e0e258a9957"
   },
   {
     "url": "index.html",
-    "revision": "14880b9b22d881f98beb8c0734ec91e8"
+    "revision": "283ee8b7dc54805285f3c1840c666228"
   },
   {
     "url": "categories/code/index.html",
-    "revision": "8ac538a5e33c88012c1d1a67784c1a3f"
+    "revision": "f415db79c4df9297a1d7e497bbb156f2"
   },
   {
     "url": "categories/index.html",
-    "revision": "e99e8ed1b5fc64fcead32950a24d97df"
+    "revision": "a864f6c5ba7b0096084810a9f3a5db0d"
   },
   {
     "url": "categories/personal/index.html",
-    "revision": "9c3f5bebe5325e52946e89416a35c8ad"
+    "revision": "0761bb55ca439afbfbbe562b5c0ffbcc"
   },
   {
     "url": "cloudflare-fast-google-fonts-service-worker-with-google-amp-custom-style-tag/index.html",
-    "revision": "5b75e0a7fd558da1366bbe5bf9c5a3e4"
+    "revision": "40ef7525aad472ae0e0f1a8974300850"
   },
   {
     "url": "creating-google-sheets-web-app-for-catching-netlify-webhooks/index.html",
-    "revision": "dcadff32d71d19449b18d68db0076868"
+    "revision": "8d66fba9e719689dc3d640758db39b85"
   },
   {
     "url": "digital-footprint/index.html",
-    "revision": "a74d823eb843c56f7b582fc071240578"
+    "revision": "ae99269a16ae129ae020b63ddebc63aa"
   },
   {
     "url": "enable-term-jquery-in-vue-cli-3-scaffolded-project/index.html",
-    "revision": "cdc876eb4c990547d373eeea73facab7"
+    "revision": "cd9487b88252a9d40a61eec015552cf9"
   },
   {
     "url": "hugo-tachyons-src-without-gulp-or-webpack/index.html",
-    "revision": "12b07ea9cb95758da00924812f26f3f2"
+    "revision": "c4fc00e674d27af3b2511177dc045bd0"
   },
   {
     "url": "lambda-function-on-netlify-as-endpoint-for-google-amp-form/index.html",
-    "revision": "0fbc98d3e1295cd9f0cddcda263e275e"
+    "revision": "2de2b9ad58a58090c5c8d12b432b42d5"
   },
   {
     "url": "page/1/index.html",
@@ -75,23 +75,23 @@ self.__precacheManifest = [
   },
   {
     "url": "page/2/index.html",
-    "revision": "3b70dcf24da8946a5006d8464edae334"
+    "revision": "4115692e16b0b73172c257136a7a521b"
   },
   {
     "url": "show-remaining-characters-in-textarea-using-amp-bind/index.html",
-    "revision": "bdf76dcb9c61fd70a2c5a0c0fb646004"
+    "revision": "5aeb013a55af18cc61c692927b02b520"
   },
   {
     "url": "uses/index.html",
-    "revision": "46e5e5b68e623243534fa904cb5cbfdd"
+    "revision": "40c167a715426da5e8c93a6ae3cde416"
   },
   {
     "url": "using-hugo-htmlunescape-function-to-solve-selector-expected-errors/index.html",
-    "revision": "3e28a5efe40b7e012ad4ee1823aee8bc"
+    "revision": "e857af010bcd0a71dfd27c6a95ad485c"
   },
   {
     "url": "welcome-to-my-site/index.html",
-    "revision": "76e6a2112094622a0e1cc449ffa041bf"
+    "revision": "b63011fb0ac33312bc41de6e6405d401"
   },
   {
     "url": "logo.webp",
